0a980a61b4a19f4aa8e638dee23fdaec01a5ad05
Carlton Joseph <carlton.joseph@gmail.com>
2019-11-21 00:40:39 -0800

    add call for sw to skip waiting

--- src/index.js
+++ src/index.js
@@ -8,4 +8,14 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: reg => {
+    console.log("reg", reg);
+    console.log("installing", reg.installing);
+    console.log("waiting", reg.waiting);
+    console.log("active", reg.active);
+    const sw = reg.waiting;
+    if (!sw) return;
+    sw.postMessage({ type: "SKIP_WAITING" });
+  }
+});
